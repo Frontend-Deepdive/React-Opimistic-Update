aca5de32d6a8c8c9c3295b0d8af1485e6a0dc8c6
silverain <ekdldkaa@naver.com>
2025-04-07 14:23:07 +0900

    add : test notion api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
-import './App.css';
+import "./App.css";
+import { Client } from "@notionhq/client";
 
 function App() {
+  const notionToken = "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";
+  const notion = new Client({
+    auth: notionToken,
+  });
+  const databaseId = "1ce888e3b219805db30bda19ad72e632";
+
+  const fetchData = async () => {
+    const dbObjects = await notion.databases.retrieve({
+      database_id: databaseId,
+    });
+    const dbQueryData = await notion.databases.query({
+      database_id: databaseId,
+    });
+
+    console.log(dbObjects);
+    console.log(dbQueryData);
+  };
+
+  fetchData();
   return <div>this is me</div>;
 }
 
