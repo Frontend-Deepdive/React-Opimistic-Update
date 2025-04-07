5b124798822830a626f32c3ec7c25ce336996f7e
silverain <ekdldkaa@naver.com>
2025-04-07 14:27:53 +0900

    fix : 형식 단언 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import "./App.css";
 import { Client } from "@notionhq/client";
 
 function App() {
-  const notionToken = process.env.REACT_APP_NOTION_TOKEN;
+  const notionToken = process.env.REACT_APP_NOTION_TOKEN as string;
   const notion = new Client({
     auth: notionToken,
   });
-  const databaseId = process.env.REACT_APP_NOTION_DATABASE_ID;
+  const databaseId = process.env.REACT_APP_NOTION_DATABASE_ID as string;
 
   const fetchData = async () => {
     const dbObjects = await notion.databases.retrieve({
