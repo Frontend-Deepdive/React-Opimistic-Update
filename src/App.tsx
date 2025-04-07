4a622032a808d4469bc1821d377c2ed4f6aac947
abyss-s <lh3729@naver.com>
2025-04-08 00:58:08 +0900

    Feat: Card 컴포넌트 구현 및 렌더링
    - id가 사이다일때, 낙관적 업데이트로 좋아요를 업데이트
    - id가 쭈니일때, 낙관적 업데이트가 실패하면 3초 뒤 롤백

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import './App.css';
+import Card from './components/Card';
 
 function App() {
-  return <div>this is me</div>;
+  return (
+    <>
+      <h1>낙관적 업데이트를 구현해봅시다</h1>
+      <h2>좋아요 버튼을 눌러보세요!</h2>
+      <p>낙관적으로 성공해요</p>
+      <Card initialLikes={0} isError={false} id='cider' />
+      <p>낙관적으로 3초 뒤 실패해요</p>
+      <Card initialLikes={0} isError={true} id='juni' />
+    </>
+  );
 }
 
 export default App;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,1 +1,62 @@
-// 카드 UI 컴포넌트 (좋아요 버튼 포함)
+import React from 'react';
+import './Card.css';
+import imgArr from '../constants/img.json';
+import useStore from '../stores/useStore';
+
+interface CardProps {
+  initialLikes: number;
+  isError: boolean;
+  id: string;
+}
+
+const Card: React.FC<CardProps> = ({ isError, id }) => {
+  const { likes, incrementLikes, decrementLikes } = useStore();
+  const [liked, setLiked] = React.useState(false);
+
+  const handleLike = async () => {
+    // 좋아요 상태를 토글
+    if (liked) {
+      decrementLikes(id);
+      setLiked(false);
+      return;
+    }
+    incrementLikes(id);
+    setLiked(true);
+
+    try {
+      if (isError) {
+        await new Promise((resolve) => {
+          setTimeout(resolve, 3000);
+          console.log('낙관적 업데이트 중... 3초 대기');
+        });
+        decrementLikes(id);
+        setLiked(false);
+        console.log('낙관적 업데이트로 좋아요가 취소됩니다!');
+      }
+    } catch (error) {}
+  };
+
+  return (
+    <div className='card'>
+      <div className='card-img'>
+        {!isError ? (
+          <img src={imgArr.imgArr[0]} alt='cider' />
+        ) : (
+          <img src={imgArr.imgArr[1]} alt='juni' />
+        )}
+      </div>
+      <div>
+        {id == 'cider' ? '사이다' : '쭈니'}를 {likes[id] || 0}명의 사람이 좋아합니다.
+      </div>
+      <button className='like-button' onClick={handleLike}>
+        {liked ? (
+          <i className='fa-solid fa-heart' style={{ color: '#ff4255' }}></i>
+        ) : (
+          <i className='fa-solid fa-heart' style={{ color: '#ebebeb' }}></i>
+        )}
+      </button>
+    </div>
+  );
+};
+
+export default Card;
