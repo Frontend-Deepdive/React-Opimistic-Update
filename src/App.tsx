47fefb0f77030690adf4ca984815afdff55c3105
silverain <ekdldkaa@naver.com>
2025-04-08 13:39:01 +0900

    feat : split card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,59 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { fetchNotionData, increaseLike } from "./api/notion";
+import { useOptimistic, useState } from "react";
+import { Card } from "./components/Card";
 
 function App() {
   const queryClient = useQueryClient();
+
+  /**
+   * 데이터 패칭
+   */
 
   const { data, isLoading, error } = useQuery({
     queryKey: ["notionData"],
     queryFn: fetchNotionData,
   });
-
   const mutation = useMutation({
     mutationFn: (pageId: string) => increaseLike(pageId),
     onSuccess: () => {
-      // 서버에 업데이트가 성공하면 다시 데이터 refetch
       queryClient.invalidateQueries({ queryKey: ["notionData"] });
     },
   });
+
+  /**
+   * 낙관적 likes 업데이트
+   */
+  const [likes, setLikes] = useState<Record<string, number>>({});
+  const [optimisticLikes, addOptimisticLike] = useOptimistic<
+    Record<string, number>,
+    { id: string; currentLikes: number }
+  >(likes, (state, newLike) => ({
+    ...state,
+    [newLike.id]: (state[newLike.id] ?? newLike.currentLikes) + 1,
+  }));
+
+  const handleLike = (id: string, currentLikes: number) => {
+    // 1. UI에 먼저 반영
+    addOptimisticLike({ id, currentLikes });
+    // 2. 서버 요청 보내기
+    mutation.mutate(id);
+  };
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error occurred!</div>;
 
   return (
     <div>
-      <h1>This is me</h1>
       <ul>
         {data?.map((item) => (
-          <li key={item.id}>
-            {item.text} - 좋아요: {item.likes}
-            <button
-              style={{ marginLeft: "10px" }}
-              onClick={() => mutation.mutate(item.id)}
-            >
-              ❤️ Like
-            </button>
-          </li>
+          <Card
+            key={item.id}
+            id={item.id}
+            text={item.text}
+            likes={optimisticLikes[item.id] ?? item.likes}
+            onLike={handleLike}
+          />
         ))}
       </ul>
     </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,1 +1,18 @@
-// 카드 UI 컴포넌트 (좋아요 버튼 포함)
+interface CardProps {
+  id: string;
+  text: string;
+  likes: number | null;
+  onLike: (id: string, currentLikes: number) => void;
+}
+export const Card = (props: CardProps) => {
+  const { id, text, likes, onLike } = props;
+
+  return (
+    <li key={id}>
+      {text} - 좋아요: {likes}
+      <button style={{ marginLeft: "10px" }} onClick={() => onLike(id, likes!)}>
+        ❤️ Like
+      </button>
+    </li>
+  );
+};
